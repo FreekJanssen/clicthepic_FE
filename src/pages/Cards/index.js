4262c6833bea59764265bbb57bfcb7d3b745e015
ewami <efili1989@gmail.com>
2020-06-30 22:12:03 +0200

    changes in css

--- src/pages/Cards/index.js
+++ src/pages/Cards/index.js
@@ -1,75 +1,81 @@
-import React from "react";
-import Form from "react-bootstrap/Form";
+import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import { useDispatch } from "react-redux";
-import { fetchCards } from "../../store/cards/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCards, setCards } from "../../store/cards/actions";
+import { selectCards } from "../../store/cards/selectors";
+
 import "./index.css";
 
 export default function Cards() {
   const dispatch = useDispatch();
+  const cards = useSelector(selectCards);
 
   const findCard = (category) => {
     dispatch(fetchCards(category));
   };
 
+  //effect with cleanup:
+  useEffect(() => {
+    return () => {
+      dispatch(setCards([]));
+    };
+  }, []);
+
   return (
     <div>
-      <h2 className="head">flipping cards</h2>
-      <div className="all">
-        <Container className="container">
-          <Form.Group controlId="formBasicName">
-            <Form.Label>Choose category</Form.Label>
-            <div className="categoryButtons">
-              <Form.Group className="mt-5">
-                <Button
-                  className="oneButton"
-                  variant="info"
-                  type="button"
-                  onClick={(event) => findCard(event.currentTarget.innerText)}
-                >
-                  Activities
-                </Button>
-              </Form.Group>
-              <Form.Group className="mt-5">
-                <Button
-                  className="oneButton"
-                  variant="warning"
-                  type="button"
-                  onClick={(event) => findCard(event.currentTarget.innerText)}
-                >
-                  Jobs
-                </Button>
-              </Form.Group>
-              <Form.Group className="mt-5">
-                <Button
-                  className="oneButton"
-                  variant="success"
-                  type="button"
-                  onClick={(event) => findCard(event.currentTarget.innerText)}
-                >
-                  Places
-                </Button>
-              </Form.Group>
-            </div>
-          </Form.Group>
-        </Container>
-      </div>
-      <div className="cardsArea">
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">
-              <img
-                className="picture"
-                src="https://cdn.pixabay.com/photo/2020/02/17/15/05/fair-4856748_1280.jpg"
-                alt="Illustration"
-              />
-            </div>
-            <div className="flip-card-back">
-              <h1 className="txtOnImg">TEXT</h1>
-            </div>
-          </div>
+      <h2 className="head">Flip the Pic</h2>
+      <div className="">
+        <p className="chooseCategory">choose category</p>
+        <div className="container">
+          <Button
+            className="oneButton"
+            variant="info"
+            type="button"
+            onClick={(event) => findCard(event.currentTarget.innerText)}
+          >
+            Activities
+          </Button>
+
+          <Button
+            className="oneButton"
+            variant="warning"
+            type="button"
+            onClick={(event) => findCard(event.currentTarget.innerText)}
+          >
+            Jobs
+          </Button>
+
+          <Button
+            className="oneButton"
+            variant="success"
+            type="button"
+            onClick={(event) => findCard(event.currentTarget.innerText)}
+          >
+            Places
+          </Button>
         </div>
+      </div>
+      <div className="cardsAreaWrapper">
+        {cards.length > 0 &&
+          cards.map((item) => (
+            <div className="cardsArea" key={item.id}>
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img
+                      className="picture"
+                      src={item.image}
+                      alt="Illustration"
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <h3 className="txtOnImg">{item.word}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
