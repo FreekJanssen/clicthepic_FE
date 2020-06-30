477deb19ae67be8102eb6df33ef985dced4b40e0
ewami <efili1989@gmail.com>
2020-06-30 13:18:38 +0200

    add css

--- src/pages/Cards/index.js
+++ src/pages/Cards/index.js
@@ -7,35 +7,39 @@ import "./index.css";
 
 export default function Cards() {
   return (
-    <div className="all">
+    <>
       <h2 className="head">flipping cards</h2>
-      <Container className="container">
-        <Form as={Col} xs={6} sm={6} md={5} lg={4} xl={4}>
-          <Form.Group controlId="formBasicName">
-            <Form.Label>Choose category</Form.Label>
-            <Form.Group className="mt-5">
-              <Button variant="primary" type="submit">
-                Clothes{" "}
-              </Button>
+      <div className="all">
+        <Container className="container">
+          <Form as={Col} xs={6} sm={6} md={5} lg={4} xl={4}>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Choose category</Form.Label>
+              <div className="categoryButtons">
+                <Form.Group className="mt-5">
+                  <Button className="oneButton" variant="primary" type="submit">
+                    Clothes{" "}
+                  </Button>
+                </Form.Group>
+                <Form.Group className="mt-5">
+                  <Button className="oneButton" variant="info" type="submit">
+                    Transport{" "}
+                  </Button>
+                </Form.Group>
+                <Form.Group className="mt-5">
+                  <Button className="oneButton" variant="warning" type="submit">
+                    Items{" "}
+                  </Button>
+                </Form.Group>
+                <Form.Group className="mt-5">
+                  <Button className="oneButton" variant="success" type="submit">
+                    Sport{" "}
+                  </Button>
+                </Form.Group>
+              </div>
             </Form.Group>
-            <Form.Group className="mt-5">
-              <Button variant="info" type="submit">
-                Transport{" "}
-              </Button>
-            </Form.Group>
-            <Form.Group className="mt-5">
-              <Button variant="warning" type="submit">
-                Items{" "}
-              </Button>
-            </Form.Group>
-            <Form.Group className="mt-5">
-              <Button variant="success" type="submit">
-                Sport{" "}
-              </Button>
-            </Form.Group>
-          </Form.Group>
-        </Form>
-      </Container>
-    </div>
+          </Form>
+        </Container>
+      </div>
+    </>
   );
 }
