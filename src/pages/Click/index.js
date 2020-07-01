e1f04be4aa9f607eabf6a0fdd73d0c8f9ec7211f
FreekJanssen <freekwjanssen@gmail.com>
2020-07-01 13:09:50 +0200

    some QOL with displaying loading, fix dependency error

--- src/pages/Click/index.js
+++ src/pages/Click/index.js
@@ -1,33 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useSelector } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 import { selectLanguage } from '../../store/appState/selectors';
 
+import Loading from '../../components/Loading';
 
 export default function Click() {
+
   const [state, setState] = useState({})
   const [randomTag, setRandomTag] = useState([]);
   const [msg, setMsg] = useState('');
   const [count, setCount] = useState(1);
   const [score, setScore] = useState(0);
 
-  const imaggaKey = process.env.REACT_APP_IMAGGA_KEY;
   const randomAnimal = 'https://source.unsplash.com/500x300/?animal';
   const randomFood = 'https://source.unsplash.com/500x300/?food';
-  const randomClothes = 'https://source.unsplash.com/500x300/?clothes';
+  //const randomClothes = 'https://source.unsplash.com/500x300/?clothes';
   const language = useSelector(selectLanguage);
-
-  const axiosConfig = { 
-    headers: { Authorization: `Basic ${imaggaKey}` },
-    params: { limit: 3, language: language }
-  };
-
+  
   useEffect(() => {
 
-    async function getImageTags() {
+    const imaggaKey = process.env.REACT_APP_IMAGGA_KEY;
+    //axios configuration object with auth header, 
+    //limit (otherwise it fetches 100 tags),
+    //and language code
+    const axiosConfig = { 
+      headers: { Authorization: `Basic ${imaggaKey}` },
+      params: { limit: 3, language: language }
+    };
 
+    async function getImageTags() {
+      
       try {
+
         //get 3 random images, 1 of each category
         const imageResponses = await Promise.all([
           await axios.get(`${randomAnimal}`),
@@ -43,7 +50,7 @@ export default function Click() {
           axios.get(`https://api.imagga.com/v2/tags?image_url=${imageURLS[1]}`, axiosConfig),
           //axios.get(`https://api.imagga.com/v2/tags?image_url=${imageURLS[2]}`, axiosConfig)
         ]);
-        console.log(tagsResponses);
+
         //create arrays with tags
         const imageTags = tagsResponses.map(res => res.data.result.tags);
         const animalTags = imageTags[0].map(tag => tag.tag[language]);
@@ -58,18 +65,21 @@ export default function Click() {
           food: { url: imageURLS[1], tags: [...foodTags] }
           //clothes: { url: imageURLS[2], tags: imageTags[2] }
         });
-        
+
       }catch(e){
         console.log(e);
       };
     };
     getImageTags();
-  },[count]);
+  },[count, language]);
 
   function nextRound(){
     setCount(count+1);
     setMsg('');
+    setState({});
+    setRandomTag('');
   }
+
   function clickedImage(e){
     if(msg) return;
     if(state[e.target.alt].tags.includes(randomTag)) {
@@ -78,17 +88,22 @@ export default function Click() {
     }
     else setMsg('Incorrect');
   }
-  if(!state.animal) return <div>Loading..</div>;
+
   if(count >= 6) return <div><h1>Finished! <br/>Your Score: {score}/5</h1></div>
+  //if(msg === 'Incorrect') return <h1>GAME OVER</h1>
 
   return (
     <div>
       <h1>Click the Pic -- Round {count}/5</h1>
       <h1>{randomTag}</h1>
-      <img src={state.animal.url} alt='animal' onClick={clickedImage}/>
-      <img src={state.food.url} alt='food' onClick={clickedImage}/>
+      {state.animal 
+      ? <div>
+          <img src={state.animal.url} alt='animal' onClick={clickedImage}/>
+          <img src={state.food.url} alt='food' onClick={clickedImage}/>
+        </div>
+      : <Loading />}
       <h2>{msg}</h2>
-      <button onClick={nextRound}>NEXT</button>
+      <Button disabled={!msg} onClick={nextRound}>NEXT</Button>
     </div>
   );
 }
