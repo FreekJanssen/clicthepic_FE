ad47c2931744ab53acb697f11752261abae4da0b
ewami <efili1989@gmail.com>
2020-07-01 17:18:47 +0200

    add content for About page, put component in the route, add css, link to our repos

--- src/pages/Click/index.js
+++ src/pages/Click/index.js
@@ -1,32 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
-
-import { selectLanguage } from '../../store/appState/selectors';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { selectLanguage } from "../../store/appState/selectors";
+import "./index.css";
 
 export default function Click() {
-  const [state, setState] = useState({})
+  const [state, setState] = useState({});
   const [randomTag, setRandomTag] = useState([]);
-  const [msg, setMsg] = useState('');
+  const [msg, setMsg] = useState("");
   const [count, setCount] = useState(1);
   const [score, setScore] = useState(0);
 
   const imaggaKey = process.env.REACT_APP_IMAGGA_KEY;
-  const randomAnimal = 'https://source.unsplash.com/500x300/?animal';
-  const randomFood = 'https://source.unsplash.com/500x300/?food';
-  const randomClothes = 'https://source.unsplash.com/500x300/?clothes';
+  const randomAnimal = "https://source.unsplash.com/500x300/?animal";
+  const randomFood = "https://source.unsplash.com/500x300/?food";
+  const randomClothes = "https://source.unsplash.com/500x300/?clothes";
   const language = useSelector(selectLanguage);
 
-  const axiosConfig = { 
+  const axiosConfig = {
     headers: { Authorization: `Basic ${imaggaKey}` },
-    params: { limit: 3, language: language }
+    params: { limit: 3, language: language },
   };
 
   useEffect(() => {
-
     async function getImageTags() {
-
       try {
         //get 3 random images, 1 of each category
         const imageResponses = await Promise.all([
@@ -35,19 +32,25 @@ export default function Click() {
           //await axios.get(`${randomClothes}`)
         ]);
         //get the actual URLs in an array
-        const imageURLS = imageResponses.map(res => res.request.responseURL);
+        const imageURLS = imageResponses.map((res) => res.request.responseURL);
 
         //Generate tags for the random images using Imagga
         const tagsResponses = await Promise.all([
-          axios.get(`https://api.imagga.com/v2/tags?image_url=${imageURLS[0]}`, axiosConfig),
-          axios.get(`https://api.imagga.com/v2/tags?image_url=${imageURLS[1]}`, axiosConfig),
+          axios.get(
+            `https://api.imagga.com/v2/tags?image_url=${imageURLS[0]}`,
+            axiosConfig
+          ),
+          axios.get(
+            `https://api.imagga.com/v2/tags?image_url=${imageURLS[1]}`,
+            axiosConfig
+          ),
           //axios.get(`https://api.imagga.com/v2/tags?image_url=${imageURLS[2]}`, axiosConfig)
         ]);
         console.log(tagsResponses);
         //create arrays with tags
-        const imageTags = tagsResponses.map(res => res.data.result.tags);
-        const animalTags = imageTags[0].map(tag => tag.tag[language]);
-        const foodTags = imageTags[1].map(tag => tag.tag[language]);
+        const imageTags = tagsResponses.map((res) => res.data.result.tags);
+        const animalTags = imageTags[0].map((tag) => tag.tag[language]);
+        const foodTags = imageTags[1].map((tag) => tag.tag[language]);
         const tagArray = [...animalTags, ...foodTags];
 
         //grab a random index of the tags array
@@ -55,38 +58,44 @@ export default function Click() {
 
         setState({
           animal: { url: imageURLS[0], tags: [...animalTags] },
-          food: { url: imageURLS[1], tags: [...foodTags] }
+          food: { url: imageURLS[1], tags: [...foodTags] },
           //clothes: { url: imageURLS[2], tags: imageTags[2] }
         });
-        
-      }catch(e){
+      } catch (e) {
         console.log(e);
-      };
-    };
-    getImageTags();
-  },[count]);
-
-  function nextRound(){
-    setCount(count+1);
-    setMsg('');
-  }
-  function clickedImage(e){
-    if(msg) return;
-    if(state[e.target.alt].tags.includes(randomTag)) {
-       setMsg('Correct!');
-       setScore(score+1);
+      }
     }
-    else setMsg('Incorrect');
+    getImageTags();
+  }, [count]);
+
+  function nextRound() {
+    setCount(count + 1);
+    setMsg("");
   }
-  if(!state.animal) return <div>Loading..</div>;
-  if(count >= 6) return <div><h1>Finished! <br/>Your Score: {score}/5</h1></div>
+  function clickedImage(e) {
+    if (msg) return;
+    if (state[e.target.alt].tags.includes(randomTag)) {
+      setMsg("Correct!");
+      setScore(score + 1);
+    } else setMsg("Incorrect");
+  }
+  if (!state.animal) return <div>Loading..</div>;
+  if (count >= 6)
+    return (
+      <div>
+        <h1>
+          Finished! <br />
+          Your Score: {score}/5
+        </h1>
+      </div>
+    );
 
   return (
     <div>
       <h1>Click the Pic -- Round {count}/5</h1>
       <h1>{randomTag}</h1>
-      <img src={state.animal.url} alt='animal' onClick={clickedImage}/>
-      <img src={state.food.url} alt='food' onClick={clickedImage}/>
+      <img src={state.animal.url} alt="animal" onClick={clickedImage} />
+      <img src={state.food.url} alt="food" onClick={clickedImage} />
       <h2>{msg}</h2>
       <button onClick={nextRound}>NEXT</button>
     </div>
