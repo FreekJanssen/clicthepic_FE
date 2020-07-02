91b761d19b706aea4c16a5882644008a4f683a55
ewami <efili1989@gmail.com>
2020-07-02 11:04:53 +0200

    add scores of user from scoreList (state)

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -5,11 +5,19 @@ import "./index.css";
 
 export default function Account() {
   const user = useSelector(selectUser);
+  console.log("here", user.scoreList);
 
   return (
     <div>
       <h1 className="head">Hello {user.name}!</h1>
       <h5 className="scoresHead">Your scores</h5>
+      <div className="scoreList">
+        <hr className="hrLine"></hr>
+        <p>English: {user?.scoreList?.en} points</p>
+        <p>German: {user?.scoreList?.de} points</p>
+        <p>Spanish: {user?.scoreList?.es} points</p>
+        <p>French: {user?.scoreList?.fr} points</p>
+      </div>
     </div>
   );
 }
