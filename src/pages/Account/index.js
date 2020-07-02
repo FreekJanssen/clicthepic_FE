a7a05f0403037d891a94be806686789a8edfcd38
ewami <efili1989@gmail.com>
2020-07-02 17:09:53 +0200

    minor visual changes

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -7,15 +7,17 @@ export default function Account() {
   const user = useSelector(selectUser);
 
   return (
-    <div>
+    <div className="entireAccountArea">
       <h1 className="head">Hello {user.name}!</h1>
-      <h5 className="scoresHead">Your scores</h5>
-      <div className="scoreList">
-        <hr className="hrLine"></hr>
-        <p>English: {user?.scoreList?.en} points</p>
-        <p>German: {user?.scoreList?.de} points</p>
-        <p>Spanish: {user?.scoreList?.es} points</p>
-        <p>French: {user?.scoreList?.fr} points</p>
+      <div className="scoreTable">
+        <h4 className="scoresHead">Your scores</h4>
+        <div className="scoreList">
+          <hr className="hrLine"></hr>
+          <p>English: {user?.scoreList?.en} points</p>
+          <p>German: {user?.scoreList?.de} points</p>
+          <p>Spanish: {user?.scoreList?.es} points</p>
+          <p>French: {user?.scoreList?.fr} points</p>
+        </div>
       </div>
     </div>
   );
