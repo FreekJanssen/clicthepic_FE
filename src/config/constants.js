9c75a7e87d9f79091789410b9b1db8f105cb72db
ewami <efili1989@gmail.com>
2020-07-03 12:52:30 +0200

    add new heroku host

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl = "https://click-the-pic-app.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 5000;
 export const IMAGGA_KEY = process.env.REACT_APP_IMAGGA_KEY;
